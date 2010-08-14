dfd4abbb9c863d1e79286480e41ac42a1d6ccb8c
Joachim Nilsson <troglobit@vmlinux.org>
2010-08-14 17:54:27 +0200

    Fix file paths on Linux builds

--- pathnames.h
+++ pathnames.h
@@ -5,19 +5,20 @@
  *
  * The mrouted program is COPYRIGHT 1989 by The Board of Trustees of
  * Leland Stanford Junior University.
- *
- *
- * pathnames.h,v 3.8 1995/11/29 22:36:57 fenner Rel
  */
+#ifndef __MROUTED_PATHNAMES_H__
+#define __MROUTED_PATHNAMES_H__
 
 #define _PATH_MROUTED_CONF	"/etc/mrouted.conf"
 
-#if (defined(BSD) && (BSD >= 199103))
+#if (defined(BSD) && (BSD >= 199103)) || defined(__linux__)
 #define _PATH_MROUTED_GENID	"/var/run/mrouted.genid"
 #define _PATH_MROUTED_DUMP	"/var/tmp/mrouted.dump"
 #define _PATH_MROUTED_CACHE	"/var/tmp/mrouted.cache"
-#else
+#else  /* Really old system ... */
 #define _PATH_MROUTED_GENID	"/etc/mrouted.genid"
 #define _PATH_MROUTED_DUMP	"/usr/tmp/mrouted.dump"
 #define _PATH_MROUTED_CACHE	"/usr/tmp/mrouted.cache"
 #endif
+
+#endif /* __MROUTED_PATHNAMES_H__ */
