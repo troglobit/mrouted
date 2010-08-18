f903c40ecffe6f058f517834491e4ae0f7782993
Joachim Nilsson <troglobit@vmlinux.org>
2010-08-18 02:05:38 +0200

    Update Debian start script, peeking a lot at pimd.

--- debian/init.d
+++ debian/init.d
@@ -1,6 +1,12 @@
-#!/bin/sh
+#! /bin/sh
 #
-# This file was automatically customized by debmake on Mon,  1 Mar 1999 21:36:23 +0100
+### BEGIN INIT INFO
+# Provides:          mrouted
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
 #
 # Written by Miquel van Smoorenburg <miquels@cistron.nl>.
 # Modified for Debian GNU/Linux by Ian Murdock <imurdock@gnu.org>.
@@ -8,33 +14,46 @@
 
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 DAEMON=/usr/sbin/mrouted
-# The following value is extracted by debstd to figure out how to generate
-# the postinst script. Edit the field to change the way the script is
-# registered through update-rc.d (see the manpage for update-rc.d!)
-FLAGS="defaults 50"
+NAME=mrouted
+DESC=mrouted
 
 test -f $DAEMON || exit 0
 
 case "$1" in
-  start)
-    insmod -k ipip 2> /dev/null || true
-    start-stop-daemon --start --verbose --exec $DAEMON
-    ;;
-  stop)
-    start-stop-daemon --stop --verbose --exec $DAEMON
-    ;;
-  reload|force-reload)
-    start-stop-daemon --stop --signal 1 --verbose --exec $DAEMON
-    ;;
-  restart)
-    start-stop-daemon --stop --verbose --exec $DAEMON
-    sleep 1
-    start-stop-daemon --start --verbose --exec $DAEMON
-    ;;
-  *)
-    echo "Usage: /etc/init.d/mrouted {start|stop|restart|force-reload}"
-    exit 1
-    ;;
+        start)
+                echo -n "Starting $DESC: "
+                modprobe ipip 2> /dev/null || true
+                start-stop-daemon --start --quiet --pidfile /var/run/$NAME.pid \
+                        --exec $DAEMON
+                echo "$NAME."
+                ;;
+        stop)
+                echo -n "Stopping $DESC: "
+                start-stop-daemon --stop --quiet --pidfile /var/run/$NAME.pid \
+                        --exec $DAEMON
+                echo "$NAME."
+                ;;
+        reload|force-reload)
+                echo -n "Reloading $DESC: "
+                start-stop-daemon --stop --signal 1 --quiet --pidfile /var/run/$NAME.pid \
+                        --exec $DAEMON
+                echo "$NAME."
+                ;;
+        restart)
+                
+                echo -n "Restarting $DESC: "
+                start-stop-daemon --stop --quiet --pidfile \
+                        /var/run/$NAME.pid --exec $DAEMON
+                sleep 1
+                start-stop-daemon --start --quiet --pidfile \
+                        /var/run/$NAME.pid --exec $DAEMON
+                echo "$NAME."
+                ;;
+        *)
+                N=/etc/init.d/$NAME
+                echo "Usage: $N {start|stop|restart|reload|force-reload}"
+                exit 1
+                ;;
 esac
 
 exit 0
