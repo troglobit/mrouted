ba4d43b074727ae27f70176e9cf3f50ebe896be7
Joachim Wiberg <troglobit@gmail.com>
2020-09-24 13:37:00 +0200

    Minor, drop typecasting of pointer not needed
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/kern.c
+++ src/kern.c
@@ -19,7 +19,7 @@ void k_init_dvmrp(void)
 {
     int v = 1;
 
-    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_INIT, (char *)&v, sizeof(int)) < 0) {
+    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_INIT, &v, sizeof(int)) < 0) {
 	if (errno == EADDRINUSE)
 	    logit(LOG_ERR, 0, "Another multicast routing application is already running.");
 	else
@@ -34,7 +34,7 @@ void k_init_dvmrp(void)
  */
 void k_stop_dvmrp(void)
 {
-    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DONE, (char *)NULL, 0) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DONE, NULL, 0) < 0)
 	logit(LOG_WARNING, errno, "Cannot disable multicast routing in kernel");
 }
 
@@ -53,14 +53,14 @@ void k_set_rcvbuf(int bufsize, int minsize)
      * to try to find the highest acceptable value.  The highest acceptable value
      * being smaller than minsize is a fatal error.
      */
-    if (setsockopt(igmp_socket, SOL_SOCKET, SO_RCVBUF, (char *)&bufsize, sizeof(bufsize)) < 0) {
+    if (setsockopt(igmp_socket, SOL_SOCKET, SO_RCVBUF, &bufsize, sizeof(bufsize)) < 0) {
         bufsize -= delta;
         while (1) {
             iter++;
             if (delta > 1)
                 delta /= 2;
 
-            if (setsockopt(igmp_socket, SOL_SOCKET, SO_RCVBUF, (char *)&bufsize, sizeof(bufsize)) < 0) {
+            if (setsockopt(igmp_socket, SOL_SOCKET, SO_RCVBUF, &bufsize, sizeof(bufsize)) < 0) {
                 bufsize -= delta;
             } else {
                 if (delta < 1024)
@@ -91,7 +91,7 @@ void k_set_rcvbuf(int bufsize, int minsize)
 void k_hdr_include(int bool)
 {
 #ifdef IP_HDRINCL
-    if (setsockopt(igmp_socket, IPPROTO_IP, IP_HDRINCL, (char *)&bool, sizeof(bool)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, IP_HDRINCL, &bool, sizeof(bool)) < 0)
         logit(LOG_ERR, errno, "Failed setting socket IP_HDRINCL %u", bool);
 #endif
 }
@@ -105,7 +105,7 @@ void k_set_ttl(int t)
     uint8_t ttl;
 
     ttl = t;
-    if (setsockopt(igmp_socket, IPPROTO_IP, IP_MULTICAST_TTL, (char *)&ttl, sizeof(ttl)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl)) < 0)
 	logit(LOG_ERR, errno, "Failed setting IP_MULTICAST_TTL %u", ttl);
 
     curttl = t;
@@ -120,7 +120,7 @@ void k_set_loop(int flag)
     uint8_t loop;
 
     loop = flag;
-    if (setsockopt(igmp_socket, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loop, sizeof(loop)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop)) < 0)
         logit(LOG_ERR, errno, "Failed setting socket IP_MULTICAST_LOOP to %u", loop);
 }
 
@@ -133,7 +133,7 @@ void k_set_if(uint32_t ifa)
     struct in_addr adr;
 
     adr.s_addr = ifa;
-    if (setsockopt(igmp_socket, IPPROTO_IP, IP_MULTICAST_IF, (char *)&adr, sizeof(adr)) < 0) {
+    if (setsockopt(igmp_socket, IPPROTO_IP, IP_MULTICAST_IF, &adr, sizeof(adr)) < 0) {
         if (errno == EADDRNOTAVAIL || errno == EINVAL)
             return;
         logit(LOG_ERR, errno, "Failed setting IP_MULTICAST_IF to %s",
@@ -152,7 +152,7 @@ void k_join(uint32_t grp, uint32_t ifa)
     mreq.imr_multiaddr.s_addr = grp;
     mreq.imr_interface.s_addr = ifa;
 
-    if (setsockopt(igmp_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&mreq, sizeof(mreq)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
 	logit(LOG_WARNING, errno, "Cannot join group %s on interface %s",
 	      inet_fmt(grp, s1, sizeof(s1)), inet_fmt(ifa, s2, sizeof(s2)));
 }
@@ -168,7 +168,7 @@ void k_leave(uint32_t grp, uint32_t ifa)
     mreq.imr_multiaddr.s_addr = grp;
     mreq.imr_interface.s_addr = ifa;
 
-    if (setsockopt(igmp_socket, IPPROTO_IP, IP_DROP_MEMBERSHIP, (char *)&mreq, sizeof(mreq)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
 	logit(LOG_WARNING, errno, "Cannot leave group %s on interface %s",
 	      inet_fmt(grp, s1, sizeof(s1)), inet_fmt(ifa, s2, sizeof(s2)));
 }
@@ -194,7 +194,7 @@ void k_add_vif(vifi_t vifi, struct uvif *v)
 
     vc.vifc_vifi = vifi;
     uvif_to_vifctl(&vc, v);
-    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_ADD_VIF, (char *)&vc, sizeof(vc)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_ADD_VIF, &vc, sizeof(vc)) < 0)
 	logit(LOG_ERR, errno, "Failed MRT_ADD_VIF(%d) for %s", vifi, v->uv_name);
 }
 
@@ -216,9 +216,9 @@ void k_del_vif(vifi_t vifi, struct uvif *v)
     vc.vifc_vifi = vifi;
     uvif_to_vifctl(&vc, v);
 
-    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DEL_VIF, (char *)&vc, sizeof(vc)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DEL_VIF, &vc, sizeof(vc)) < 0)
 #else /* *BSD et al. */
-    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DEL_VIF, (char *)&vifi, sizeof(vifi)) < 0)
+    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DEL_VIF, &vifi, sizeof(vifi)) < 0)
 #endif /* !__linux__ */
     {
         if (errno == EADDRNOTAVAIL || errno == EINVAL)
@@ -244,7 +244,7 @@ void k_add_rg(uint32_t origin, struct gtable *g)
     for (i = 0; i < numvifs; i++)
 	mc.mfcc_ttls[i] = g->gt_ttls[i];
 
-    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_ADD_MFC, (char *)&mc, sizeof(mc)) < 0) {
+    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_ADD_MFC, &mc, sizeof(mc)) < 0) {
 	char ttls[3 * MAXVIFS + 1] = { 0 };
 
 	for (i = 0; i < numvifs; i++) {
@@ -273,7 +273,7 @@ int k_del_rg(uint32_t origin, struct gtable *g)
     mc.mfcc_mcastgrp.s_addr = g->gt_mcastgrp;
 
     /* write to kernel space */
-    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DEL_MFC, (char *)&mc, sizeof(mc)) < 0) {
+    if (setsockopt(igmp_socket, IPPROTO_IP, MRT_DEL_MFC, &mc, sizeof(mc)) < 0) {
 	logit(LOG_WARNING, errno, "Failed MRT_DEL_MFC(%s %s)",
 	      inet_fmt(origin, s1, sizeof(s1)), inet_fmt(g->gt_mcastgrp, s2, sizeof(s2)));
 
@@ -291,7 +291,7 @@ int k_get_version(void)
     int vers;
     socklen_t len = sizeof(vers);
 
-    if (getsockopt(igmp_socket, IPPROTO_IP, MRT_VERSION, (char *)&vers, &len) < 0)
+    if (getsockopt(igmp_socket, IPPROTO_IP, MRT_VERSION, &vers, &len) < 0)
 	logit(LOG_ERR, errno, "Failed MRT_VERSION(): Cannot read version of multicast routing stack");
 
     return vers;
@@ -307,7 +307,7 @@ int k_get_vif_count(vifi_t vifi, int *icount, int *ocount, int *ibytes, int *oby
     int retval = 0;
 
     vreq.vifi = vifi;
-    if (ioctl(udp_socket, SIOCGETVIFCNT, (char *)&vreq) < 0) {
+    if (ioctl(udp_socket, SIOCGETVIFCNT, &vreq) < 0) {
 	logit(LOG_WARNING, errno, "SIOCGETVIFCNT on vif %d", vifi);
 	vreq.icount = vreq.ocount = vreq.ibytes =
 		vreq.obytes = 0xffffffff;
@@ -334,7 +334,7 @@ int k_get_sg_count(uint32_t src, uint32_t grp, int *pktcnt, int *bytecnt, int *w
 
     sgreq.src.s_addr = src;
     sgreq.grp.s_addr = grp;
-    if (ioctl(udp_socket, SIOCGETSGCNT, (char *)&sgreq) < 0) {
+    if (ioctl(udp_socket, SIOCGETSGCNT, &sgreq) < 0) {
 	logit(LOG_WARNING, errno, "SIOCGETSGCNT on (%s %s)",
 	    inet_fmt(src, s1, sizeof(s1)), inet_fmt(grp, s2, sizeof(s2)));
 	sgreq.pktcnt = sgreq.bytecnt = sgreq.wrong_if = 0xffffffff;
