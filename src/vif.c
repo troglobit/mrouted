troglobit/mrouted

commit b9c65cb56be5af139d003ed858e5f4f8e8b9dcdf

mrouted: Initial support for sending IGMPv3 queries
Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/vif.c
@@ -42,7 +42,7 @@ static void start_vif2(vifi_t vifi);
 static void stop_vif(vifi_t vifi);
 static void age_old_hosts(void);
 static void send_probe_on_vif(struct uvif *v);
-static void send_query(struct uvif *v);
+static void send_query(struct uvif *v, uint32_t dst, int code, uint32_t group);
 static int info_version(uint8_t *p, size_t plen);
 static void DelVif(void *arg);
 static int SetTimer(vifi_t vifi, struct listaddr *g);
@@ -313,18 +313,34 @@ static void send_probe_on_vif(struct uvif *v)
     send_on_vif(v, 0, DVMRP_PROBE, datalen);
 }
 
-static void send_query(struct uvif *v)
+static void send_query(struct uvif *v, uint32_t dst, int code, uint32_t group)
 {
-    IF_DEBUG(DEBUG_IGMP) {
-	logit(LOG_DEBUG, 0, "Sending %squery on vif %d",
-	      (v->uv_flags & VIFF_IGMPV1) ? "v1 " : "",
-	      v - uvifs);
+    int datalen = 4;
+
+    /*
+     * IGMP version to send depends on the compatibility mode of the
+     * interface:
+     *  - IGMPv2: routers MUST send Periodic Queries truncated at the
+     *    Group Address field (i.e., 8 bytes long).
+     *  - IGMPv1: routers MUST send Periodic Queries with a Max Response
+     *    Time of 0
+     */
+    if (v->uv_flags & VIFF_IGMPV2) {
+	datalen = 0;
+    } else if (v->uv_flags & VIFF_IGMPV1) {
+	datalen = 0;
+	code = 0;
     }
 
-    send_igmp(v->uv_lcl_addr, allhosts_group,
-		IGMP_MEMBERSHIP_QUERY, 
-		(v->uv_flags & VIFF_IGMPV1) ? 0 :
-		IGMP_MAX_HOST_REPORT_DELAY * IGMP_TIMER_SCALE, 0, 0);
+    IF_DEBUG(DEBUG_IGMP) {
+	logit(LOG_DEBUG, 0, "Sending %squery on %s",
+	      (v->uv_flags & VIFF_IGMPV1) ? "v1 " :
+	      (v->uv_flags & VIFF_IGMPV2) ? "v2 " : "v3 ",
+	      v->uv_name);
+    }
+
+    send_igmp(v->uv_lcl_addr, dst, IGMP_MEMBERSHIP_QUERY,
+	      code, 0, datalen);
 }
 
 /*
@@ -395,7 +411,7 @@ static void start_vif2(vifi_t vifi)
 	IF_DEBUG(DEBUG_IGMP) {
 	    logit(LOG_DEBUG, 0, "Assuming querier duties on vif %d", vifi);
 	}
-	send_query(v);
+	send_query(v, allhosts_group, IGMP_MAX_HOST_REPORT_DELAY * IGMP_TIMER_SCALE, 0);
     }
 
     v->uv_leaf_timer = LEAF_CONFIRMATION_TIME;
@@ -580,9 +596,9 @@ void query_groups(void)
     struct uvif *v;
 
     for (vifi = 0, v = uvifs; vifi < numvifs; vifi++, v++) {
-	if (v->uv_flags & VIFF_QUERIER) {
-	    send_query(v);
-	}
+	if (v->uv_flags & VIFF_QUERIER)
+	    send_query(v, allhosts_group, IGMP_MAX_HOST_REPORT_DELAY *
+		       IGMP_TIMER_SCALE, 0);
     }
 
     age_old_hosts();
@@ -825,10 +841,8 @@ void accept_leave_message(uint32_t src, uint32_t dst, uint32_t group)
 	    /** send a group specific querry **/
 	    g->al_timer = IGMP_LAST_MEMBER_QUERY_INTERVAL *
 			(IGMP_LAST_MEMBER_QUERY_COUNT + 1);
-	    send_igmp(v->uv_lcl_addr, g->al_addr,
-		        IGMP_MEMBERSHIP_QUERY, 
-		        IGMP_LAST_MEMBER_QUERY_INTERVAL * IGMP_TIMER_SCALE,
-		        g->al_addr, 0);
+	    send_query(v, g->al_addr, IGMP_LAST_MEMBER_QUERY_INTERVAL *
+		       IGMP_TIMER_SCALE, g->al_addr);
 	    g->al_query = SetQueryTimer(g, vifi,
 			IGMP_LAST_MEMBER_QUERY_INTERVAL,
 			IGMP_LAST_MEMBER_QUERY_INTERVAL * IGMP_TIMER_SCALE);
@@ -1511,7 +1525,8 @@ void age_vifs(void)
 		free(v->uv_querier);
 		v->uv_querier = NULL;
 		v->uv_flags |= VIFF_QUERIER;
-		send_query(v);
+		send_query(v, allhosts_group, IGMP_MAX_HOST_REPORT_DELAY *
+			   IGMP_TIMER_SCALE, 0);
 	    }
 	}
     }
@@ -1541,6 +1556,7 @@ static struct vnflags {
 	{ VIFF_ONEWAY,		"one-way" },
 	{ VIFF_LEAF,		"leaf" },
 	{ VIFF_IGMPV1,		"IGMPv1" },
+	{ VIFF_IGMPV2,		"IGMPv2" },
 	{ VIFF_REXMIT_PRUNES,	"rexmit_prunes" },
 	{ VIFF_PASSIVE,		"passive" },
 	{ VIFF_ALLOW_NONPRUNERS,"allow_nonpruners" },
@@ -1773,8 +1789,7 @@ static void SendQuery(void *arg)
     cbk_t *cbk = (cbk_t *)arg;
     struct uvif *v = &uvifs[cbk->vifi];
 
-    send_igmp(v->uv_lcl_addr, cbk->g->al_addr, IGMP_MEMBERSHIP_QUERY,
-	      cbk->q_time, cbk->g->al_addr, 0);
+    send_query(v, cbk->g->al_addr, cbk->q_time, cbk->g->al_addr);
     cbk->g->al_query = 0;
     free(cbk);
 }

src/defs.h
@@ -69,6 +69,7 @@ typedef void (*ihfunc_t) (int);
 
 #include "dvmrp.h"
 #include "igmpv2.h"
+#include "igmpv3.h"
 #include "vif.h"
 #include "route.h"
 #include "prune.h"

src/igmp.c
@@ -347,6 +347,119 @@ static int igmp_log_level(uint32_t type, uint32_t code)
 }
 
 /*
+ * RFC-3376 states that Max Resp Code (MRC) and Querier's Query Interval Code
+ * (QQIC) should be presented in floating point value if their value exceeds
+ * 128. The following formula is used by IGMPv3 clients to calculate the
+ * actual value of the floating point:
+ *
+ *       0 1 2 3 4 5 6 7
+ *      +-+-+-+-+-+-+-+-+
+ *      |1| exp | mant  |
+ *      +-+-+-+-+-+-+-+-+
+ *
+ *   QQI / MRT = (mant | 0x10) << (exp + 3)
+ *
+ * This requires us to find the largest set (fls) bit in the 15-bit number
+ * and set the exponent based on its index in the bits 15-8. ie.
+ *
+ *   exponent 0: igmp_fls(0000 0000 1000 0010)
+ *   exponent 5: igmp_fls(0001 0000 0000 0000)
+ *   exponent 7: igmp_fls(0111 0101 0000 0000)
+ *
+ * and set that as the exponent. The mantissa is set to the last 4 bits
+ * remaining after the (3 + exponent) shifts to the right.
+ *
+ * Note!
+ * The numbers 31744-32767 are the maximum we can present with floating
+ * point that has an exponent of 3 and a mantissa of 4. After this the
+ * implementation just wraps around back to zero.
+ */
+static inline uint8_t igmp_floating_point(unsigned int mantissa)
+{
+    unsigned int exponent;
+
+    /* Wrap around numbers larger than 2^15, since those can not be
+     * presented with 7-bit floating point. */
+    mantissa &= 0x00007FFF;
+
+    /* If top 8 bits are zero. */
+    if (!(mantissa & 0x00007F80))
+        return mantissa;
+
+    /* Shift the mantissa and mark this code floating point. */
+    mantissa >>= 3;
+    /* At this point the actual exponent (bits 7-5) are still 0, but the
+     * exponent might be incremented below. */
+    exponent   = 0x00000080;
+
+    /* If bits 7-4 are not zero. */
+    if (mantissa & 0x00000F00) {
+        mantissa >>= 4;
+        /* The index of largest set bit is at least 4. */
+        exponent  |= 0x00000040;
+    }
+
+    /* If bits 7-6 OR bits 3-2 are not zero. */
+    if (mantissa & 0x000000C0) {
+        mantissa >>= 2;
+        /* The index of largest set bit is atleast 6 if we shifted the
+         * mantissa earlier or atleast 2 if we did not shift it. */
+        exponent  |= 0x00000020;
+    }
+
+    /* If bit 7 OR bit 3 OR bit 1 is not zero. */
+    if (mantissa & 0x00000020) {
+        mantissa >>= 1;
+        /* The index of largest set bit is atleast 7 if we shifted the
+         * mantissa two times earlier or atleast 3 if we shifted the
+         * mantissa last time or atleast 1 if we did not shift it. */
+        exponent  |= 0x00000010;
+    }
+
+    return exponent | (mantissa & 0x0000000F);
+}
+
+size_t build_query(uint32_t src, uint32_t dst, int type, int code, uint32_t group, int datalen)
+{
+    struct igmpv3_query *igmp;
+    struct ip *ip;
+    size_t igmp_len = IGMP_MINLEN + datalen;
+    size_t len = MIN_IP_HEADER_LEN + igmp_len;
+
+    ip                = (struct ip *)send_buf;
+    ip->ip_src.s_addr = src;
+    ip->ip_dst.s_addr = dst;
+#ifdef HAVE_IP_HDRINCL_BSD_ORDER
+    ip->ip_len        = len;
+#else
+    ip->ip_len        = htons(len);
+#endif
+    if (IN_MULTICAST(ntohl(dst)))
+	ip->ip_ttl    = curttl;
+    else
+	ip->ip_ttl    = MAXTTL;
+
+    igmp              = (struct igmpv3_query *)(send_buf + MIN_IP_HEADER_LEN);
+    igmp->type        = type;
+    if (datalen >= 4)
+        igmp->code    = igmp_floating_point(code);
+    else
+        igmp->code    = code;
+    igmp->group       = group;
+    igmp->csum        = 0;
+
+    if (datalen >= 4) {
+        igmp->qrv     = 2;
+        igmp->qqic    = igmp_floating_point(IGMP_QUERY_INTERVAL);
+    }
+
+    /* Note: calculate IGMP checksum last. */
+    igmp->csum = inet_cksum((uint16_t *)igmp, igmp_len);
+
+    return len;
+}
+
+/*
  * Construct an IGMP message in the output packet buffer.  The caller may
  * have already placed data in that buffer, of length 'datalen'.
  */
@@ -391,7 +504,10 @@ void send_igmp(uint32_t src, uint32_t dst, int type, int code, uint32_t group, i
     size_t len;
     int rc, setloop = 0;
 
-    len = build_igmp(src, dst, type, code, group, datalen);
+    if (IGMP_MEMBERSHIP_QUERY == type)
+       len = build_query(src, dst, type, code, group, datalen);
+    else
+       len = build_igmp(src, dst, type, code, group, datalen);
 
     if (IN_MULTICAST(ntohl(dst))) {
 	k_set_if(src);

src/igmpv3.h
@@ -1,0 +1,99 @@
+/*
+ * Copyright (c) 1998-2001
+ * University of Southern California/Information Sciences Institute.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef PIMD_IGMPV3_H_
+#define PIMD_IGMPV3_H_
+
+/*
+ * IGMPv3 report modes.
+ */
+#ifndef IGMP_MODE_IS_INCLUDE
+#define IGMP_DO_NOTHING			0	/* don't send a record */
+#define IGMP_MODE_IS_INCLUDE		1	/* MODE_IN */
+#define IGMP_MODE_IS_EXCLUDE		2	/* MODE_EX */
+#define IGMP_CHANGE_TO_INCLUDE_MODE	3	/* TO_IN */
+#define IGMP_CHANGE_TO_EXCLUDE_MODE	4	/* TO_EX */
+#define IGMP_ALLOW_NEW_SOURCES		5	/* ALLOW_NEW */
+#define IGMP_BLOCK_OLD_SOURCES		6	/* BLOCK_OLD */
+#endif
+
+struct igmpv3_query {
+    uint8_t  type;
+    uint8_t  code;
+    uint16_t csum;
+    uint32_t group;
+#if defined(BYTE_ORDER) && (BYTE_ORDER == LITTLE_ENDIAN)
+    uint8_t  qrv:3,
+             suppress:1,
+             resv:4;
+#else
+    uint8_t  resv:4,
+	     suppress:1,
+	     qrv:3;
+#endif
+    uint8_t  qqic;
+    uint16_t nsrcs;
+    uint32_t srcs[0];
+};
+
+struct igmpv3_grec {
+    uint8_t  grec_type;
+    uint8_t  grec_auxwords;
+    uint16_t grec_nsrcs;
+    uint32_t grec_mca;
+    uint32_t grec_src[0];
+};
+
+#define IGMP_GRPREC_HDRLEN		8
+#define IGMP_V3_GROUP_RECORD_MIN_SIZE	8
+
+struct igmpv3_report {
+    uint8_t  type;
+    uint8_t  resv1;
+    uint16_t csum;
+    uint16_t resv2;
+    uint16_t ngrec;
+    struct igmpv3_grec grec[0];
+};
+
+#ifndef IGMP_V3_REPORT_MINLEN
+#define IGMP_V3_REPORT_MINLEN		8
+#define IGMP_V3_REPORT_MAXRECS		65535
+#endif
+
+#endif /* PIMD_IGMPV3_H_ */
+
+/**
+ * Local Variables:
+ *  indent-tabs-mode: t
+ *  c-file-style: "ellemtel"
+ *  c-basic-offset: 4
+ * End:
+ */

commit 1b81fd4a94b0b47265125c2cab39b93f46048d45

Fix issue #46: IGMP group-specific query, broken in b9c65cb
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/vif.c
@@ -371,7 +371,7 @@ static void send_query(struct uvif *v, uint32_t dst, int code, uint32_t group)
     }
 
     send_igmp(v->uv_lcl_addr, dst, IGMP_MEMBERSHIP_QUERY,
-	      code, 0, datalen);
+	      code, group, datalen);
 }
 
 /*